f4a8f7adc4b808c0a8fcdaa5d66119fa151f52a0
edaeren <edaeren2001@outlook.com>
2021-12-16 21:54:39 +0300

    ilk gönderme

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace sanatGalerisi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TxteserAdi = new System.Windows.Forms.TextBox();
+            this.TxteserTeknigi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxteserNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtturKodu = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtsanatciNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtdonemNo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1119, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1119, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1119, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Guncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1119, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Ara";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(695, 449);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 56);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Listele";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(827, 398);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // TxteserAdi
+            // 
+            this.TxteserAdi.Location = new System.Drawing.Point(969, 93);
+            this.TxteserAdi.Name = "TxteserAdi";
+            this.TxteserAdi.Size = new System.Drawing.Size(100, 22);
+            this.TxteserAdi.TabIndex = 7;
+            // 
+            // TxteserTeknigi
+            // 
+            this.TxteserTeknigi.Location = new System.Drawing.Point(969, 137);
+            this.TxteserTeknigi.Name = "TxteserTeknigi";
+            this.TxteserTeknigi.Size = new System.Drawing.Size(100, 22);
+            this.TxteserTeknigi.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(843, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Eser Adi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(844, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Eser Teknigi";
+            // 
+            // TxteserNo
+            // 
+            this.TxteserNo.Location = new System.Drawing.Point(969, 51);
+            this.TxteserNo.Name = "TxteserNo";
+            this.TxteserNo.Size = new System.Drawing.Size(100, 22);
+            this.TxteserNo.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(843, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "EserNo";
+            // 
+            // TxtturKodu
+            // 
+            this.TxtturKodu.Location = new System.Drawing.Point(969, 180);
+            this.TxtturKodu.Name = "TxtturKodu";
+            this.TxtturKodu.Size = new System.Drawing.Size(100, 22);
+            this.TxtturKodu.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(844, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "TurKodu";
+            // 
+            // TxtsanatciNo
+            // 
+            this.TxtsanatciNo.Location = new System.Drawing.Point(969, 222);
+            this.TxtsanatciNo.Name = "TxtsanatciNo";
+            this.TxtsanatciNo.Size = new System.Drawing.Size(100, 22);
+            this.TxtsanatciNo.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(845, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "SanatciNo";
+            // 
+            // TxtdonemNo
+            // 
+            this.TxtdonemNo.Location = new System.Drawing.Point(969, 267);
+            this.TxtdonemNo.Name = "TxtdonemNo";
+            this.TxtdonemNo.Size = new System.Drawing.Size(100, 22);
+            this.TxtdonemNo.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(847, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "DonemNo";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1121, 357);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 148);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Eski Ve Guncellenme Sonrasi DonemNo Bilgilerini Goster";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(1293, 533);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtdonemNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtsanatciNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtturKodu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxteserNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxteserTeknigi);
+            this.Controls.Add(this.TxteserAdi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TxteserAdi;
+        private System.Windows.Forms.TextBox TxteserTeknigi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxteserNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtturKodu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtsanatciNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtdonemNo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+
+namespace sanatGalerisi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NpgsqlConnection baglanti = new NpgsqlConnection("Server=localhost;Port=5432;Database=ArtGallery;User Id=postgres;password=leviroki55");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into \"SanatEseri\" (\"eserNo\",\"eserAdi\",\"teknik\",\"turKodu\",\"sanatciNo\",\"donemNo\") values(@p1,@p2,@p3,@p4,@p5,@p6)", baglanti);
+            komut1.Parameters.AddWithValue("@p1", int.Parse(TxteserNo.Text));
+            komut1.Parameters.AddWithValue("@p2", TxteserAdi.Text);
+            komut1.Parameters.AddWithValue("@p3", TxteserTeknigi.Text);
+            komut1.Parameters.AddWithValue("@p4", int.Parse(TxtturKodu.Text));
+            komut1.Parameters.AddWithValue("@p5", int.Parse(TxtsanatciNo.Text));
+            komut1.Parameters.AddWithValue("@p6", int.Parse(TxtdonemNo.Text));
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Eser ekleme islemi basarili.");
+        }
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            string sorgu = "select * from \"SanatEseri\"";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("Delete From \"SanatEseri\" where \"eserNo\"=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(TxteserNo.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Urun silme islemi basarili.","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("Update \"SanatEseri\" set \"eserAdi\"=@p2,\"teknik\"=@p3,\"turKodu\"=@p4,\"sanatciNo\"=@p5,\"donemNo\"=@p6 where \"eserNo\"=@p1", baglanti);
+            komut3.Parameters.AddWithValue("@p1", int.Parse(TxteserNo.Text));
+            komut3.Parameters.AddWithValue("@p2", TxteserAdi.Text);
+            komut3.Parameters.AddWithValue("@p3", TxteserTeknigi.Text);
+            komut3.Parameters.AddWithValue("@p4", int.Parse(TxtturKodu.Text));
+            komut3.Parameters.AddWithValue("@p5", int.Parse(TxtsanatciNo.Text));
+            komut3.Parameters.AddWithValue("@p6", int.Parse(TxtdonemNo.Text));
+            komut3.ExecuteNonQuery();
+            MessageBox.Show("Urun guncelleme islemi basarili.","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            baglanti.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (TxteserNo.Text != string.Empty) {
+                baglanti.Open();
+                string sorgu = "Select * From \"SanatEseri\" where \"eserNo\" =" + TxteserNo.Text;
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                baglanti.Close();
+            }
+
+            if (TxteserAdi.Text != string.Empty) {
+                baglanti.Open();
+                string sorgu2 = "Select * From \"SanatEseri\" where \"eserAdi\" " + "Like '%" + TxteserAdi.Text + "%'";
+                NpgsqlDataAdapter da2 = new NpgsqlDataAdapter(sorgu2, baglanti);
+                DataSet ds2 = new DataSet();
+                da2.Fill(ds2);
+                dataGridView1.DataSource = ds2.Tables[0];
+                baglanti.Close();
+            }
+
+            if (TxteserTeknigi.Text != string.Empty) {
+                baglanti.Open();
+                string sorgu3 = "Select * From \"SanatEseri\" where \"teknik\" " + "Like '%" + TxteserTeknigi.Text + "%'";
+                NpgsqlDataAdapter da3 = new NpgsqlDataAdapter(sorgu3, baglanti);
+                DataSet ds3 = new DataSet();
+                da3.Fill(ds3);
+                dataGridView1.DataSource = ds3.Tables[0];
+                baglanti.Close();
+            }
+
+            if (TxtturKodu.Text != string.Empty) {
+                baglanti.Open();
+                string sorgu4 = "Select * From \"SanatEseri\" where \"turKodu\"= " + TxtturKodu.Text;
+                NpgsqlDataAdapter da4 = new NpgsqlDataAdapter(sorgu4, baglanti);
+                DataSet ds4 = new DataSet();
+                da4.Fill(ds4);
+                dataGridView1.DataSource = ds4.Tables[0];
+                baglanti.Close();
+            }
+
+            if (TxtsanatciNo.Text != string.Empty) {
+                baglanti.Open();
+                string sorgu5 = "Select * From \"SanatEseri\" where \"sanatciNo\"= " + TxtsanatciNo.Text;
+                NpgsqlDataAdapter da5 = new NpgsqlDataAdapter(sorgu5, baglanti);
+                DataSet ds5 = new DataSet();
+                da5.Fill(ds5);
+                dataGridView1.DataSource = ds5.Tables[0];
+                baglanti.Close();
+            }
+
+            if (TxtdonemNo.Text != string.Empty) {
+                baglanti.Open();
+                string sorgu6 = "Select * From \"SanatEseri\" where \"donemNo\"= " + TxtdonemNo.Text;
+                NpgsqlDataAdapter da6 = new NpgsqlDataAdapter(sorgu6, baglanti);
+                DataSet ds6 = new DataSet();
+                da6.Fill(ds6);
+                dataGridView1.DataSource = ds6.Tables[0];
+                baglanti.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string sorgu7 = "select * from \"eserDegisiklikIzle\"";
+            NpgsqlDataAdapter da7 = new NpgsqlDataAdapter(sorgu7, baglanti);
+            DataSet ds7 = new DataSet();
+            da7.Fill(ds7);
+            dataGridView1.DataSource = ds7.Tables[0];
+        }
+    }
+}
